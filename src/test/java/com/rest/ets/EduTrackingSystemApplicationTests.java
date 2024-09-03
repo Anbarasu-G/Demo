9a856c957ff7c3b4a8bbf058a2376fd94248f988
Anbarasu Govindaraj <anbarasugovindaraj01@gmail.com>
2024-09-03 19:44:54 +0530

    Project Set-up completed

---
+++ src/test/java/com/rest/ets/EduTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.ets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
