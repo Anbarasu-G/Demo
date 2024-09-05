a1a55cc877f46bf05106de1a1411d483633aa735
Anbarasu Govindaraj <anbarasugovindaraj01@gmail.com>
2024-09-05 10:52:13 +0530

    yaml files configured

--- src/test/java/com/rest/ets/EduTrackingSystemApplicationTests.java
+++ src/test/java/com/rest/ets/EduTrackingSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.rest.ets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EduTrackingSystemApplicationTests {
 
 	@Test
