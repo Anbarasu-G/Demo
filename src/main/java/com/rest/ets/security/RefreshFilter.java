51bfbda5a7342ffe82542f65211bf0d005a1766d
Anbarasu Govindaraj <anbarasugovindaraj01@gmail.com>
2024-09-20 13:33:28 +0530

    RefreshFilter was Configured

---
+++ src/main/java/com/rest/ets/security/RefreshFilter.java
@@ -0,0 +1,66 @@
+package com.rest.ets.security;
+
+import com.rest.ets.enums.UserRole;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class RefreshFilter extends OncePerRequestFilter {
+    private JWT_Service  jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            System.out.println("cookies are present" + cookies.length);
+            Optional<Cookie> refreshTokenCookie = Arrays.stream(cookies)
+                    .filter(cookie -> "rt".equals(cookie.getName()))
+                    .findFirst();
+
+            if (refreshTokenCookie.isPresent()) {
+                String token = refreshTokenCookie.get().getValue();
+                if (!token.isEmpty()) {
+                    Claims claims = jwtService.parseJwt(token);
+                    String role = claims.get("role", String.class);
+                    String email = claims.get("email", String.class);
+
+                    if (role != null && email != null) {
+
+
+                        UserRole userRole = UserRole.valueOf(role);
+                        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, null,
+                                UserRole.valueOf(role)
+                                        .getPrivileges()
+                                        .stream()
+                                        .map((privilege) -> {
+                                            return new SimpleGrantedAuthority(privilege.name());
+                                        })
+                                        .toList()
+                        );
+
+                        authenticationToken.setDetails(new WebAuthenticationDetails(request));
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                    }
+                }
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/rest/ets/security/SecurityConfig.java
+++ src/main/java/com/rest/ets/security/SecurityConfig.java
@@ -4,11 +4,12 @@ package com.rest.ets.security;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,10 +24,12 @@ public class SecurityConfig {
     private JWT_Service jwtService;
 
     @Bean
+    @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatchers(matchers -> matchers.requestMatchers("/"))
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers(
                                         "/admins/register",
@@ -48,7 +51,19 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder(12);
     }
 
-
+    @Order(1)
+    @Bean
+    SecurityFilterChain refreshFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf-> csrf.disable())
+                .securityMatchers(matchers -> matchers.requestMatchers( "/refresh_login/**"))
+                .authorizeHttpRequests(authorize->
+                        authorize.anyRequest()
+                                .authenticated())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new RefreshFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
